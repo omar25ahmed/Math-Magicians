1610eef1cf89a181ca7eeb7a7b55c5269cb51eb9
Omar Ragheb <ahmed.raph29@gmail.com>
2022-03-23 03:34:38 +0200

    implementing the router feature

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import { Component } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
 import Calculator from './Components/Calculator';
+import Home from './Pages/Home';
+import Quote from './Pages/Quote';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
+const App = () => (
+  <Router>
+    <nav className="navbar">
+      <h1>Math Magicians</h1>
+      <div className="btns">
+        <Link to="/"> Home </Link>
+        <Link to="/calculator" className="u-btn"> Calculator </Link>
+        <Link to="/Quote"> Quote </Link>
       </div>
-    );
-  }
-}
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
