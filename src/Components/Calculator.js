d2e20459d612f7121c1b76448653b7c3d09bb593
Omar Ragheb <ahmed.raph29@gmail.com>
2022-03-23 03:35:44 +0200

    Changing the calculator page

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -10,78 +10,81 @@ export default function Calculator() {
   }
   const { total, next, operation } = state;
   return (
-    <div className="calculator">
-      <div className="result">
-        {total}
-        {operation}
-        {next}
-      </div>
-      <div className="row">
-        <button onClick={clickHandler} type="button">
-          AC
-        </button>
-        <button onClick={clickHandler} type="button">
-          +/-
-        </button>
-        <button onClick={clickHandler} type="button">
-          %
-        </button>
-        <button onClick={clickHandler} type="button" className="operations">
-          ÷
-        </button>
-      </div>
-      <div className="row">
-        <button onClick={clickHandler} type="button">
-          7
-        </button>
-        <button onClick={clickHandler} type="button">
-          8
-        </button>
-        <button onClick={clickHandler} type="button">
-          9
-        </button>
-        <button onClick={clickHandler} type="button" className="operations">
-          x
-        </button>
-      </div>
-      <div className="row">
-        <button onClick={clickHandler} type="button">
-          4
-        </button>
-        <button onClick={clickHandler} type="button">
-          5
-        </button>
-        <button onClick={clickHandler} type="button">
-          6
-        </button>
-        <button onClick={clickHandler} type="button" className="operations">
-          -
-        </button>
-      </div>
-      <div className="row">
-        <button onClick={clickHandler} type="button">
-          1
-        </button>
-        <button onClick={clickHandler} type="button">
-          2
-        </button>
-        <button onClick={clickHandler} type="button">
-          3
-        </button>
-        <button onClick={clickHandler} type="button" className="operations">
-          +
-        </button>
-      </div>
-      <div className="row">
-        <button onClick={clickHandler} type="button" className="last-btn">
-          0
-        </button>
-        <button onClick={clickHandler} type="button">
-          .
-        </button>
-        <button onClick={clickHandler} type="button" className="operations">
-          =
-        </button>
+    <div className="calculator-page">
+      <h1>Lets do some math :</h1>
+      <div className="calculator">
+        <div className="result">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <div className="row">
+          <button onClick={clickHandler} type="button">
+            AC
+          </button>
+          <button onClick={clickHandler} type="button">
+            +/-
+          </button>
+          <button onClick={clickHandler} type="button">
+            %
+          </button>
+          <button onClick={clickHandler} type="button" className="operations">
+            ÷
+          </button>
+        </div>
+        <div className="row">
+          <button onClick={clickHandler} type="button">
+            7
+          </button>
+          <button onClick={clickHandler} type="button">
+            8
+          </button>
+          <button onClick={clickHandler} type="button">
+            9
+          </button>
+          <button onClick={clickHandler} type="button" className="operations">
+            x
+          </button>
+        </div>
+        <div className="row">
+          <button onClick={clickHandler} type="button">
+            4
+          </button>
+          <button onClick={clickHandler} type="button">
+            5
+          </button>
+          <button onClick={clickHandler} type="button">
+            6
+          </button>
+          <button onClick={clickHandler} type="button" className="operations">
+            -
+          </button>
+        </div>
+        <div className="row">
+          <button onClick={clickHandler} type="button">
+            1
+          </button>
+          <button onClick={clickHandler} type="button">
+            2
+          </button>
+          <button onClick={clickHandler} type="button">
+            3
+          </button>
+          <button onClick={clickHandler} type="button" className="operations">
+            +
+          </button>
+        </div>
+        <div className="row">
+          <button onClick={clickHandler} type="button" className="last-btn">
+            0
+          </button>
+          <button onClick={clickHandler} type="button">
+            .
+          </button>
+          <button onClick={clickHandler} type="button" className="operations">
+            =
+          </button>
+        </div>
       </div>
     </div>
   );
