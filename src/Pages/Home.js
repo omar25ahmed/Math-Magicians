59122d293df5e3d0de00748fc8de940bb0c95f73
Omar Ragheb <ahmed.raph29@gmail.com>
2022-03-23 03:35:12 +0200

    Adding home page

---
+++ src/Pages/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-page">
+      <h1>Welcome to our page</h1>
+      <p>
+        The history of mathematics deals with the origin of discoveries
+        in mathematics and the mathematical methods and
+        notation of the past. Before the modern age and the worldwide
+        spread of knowledge, written examples of new
+        mathematical developments have come to light only in a few locales.
+        From 3000 BC the Mesopotamian states of
+        Sumer, Akkad and Assyria, followed closely by Ancient Egypt and
+        the Levantine state of Ebla began using
+        arithmetic, algebra
+        and geometry for purposes of taxation, commerce, trade and also in the patterns in nature,
+        the field of astronomy and to record time and formulate calendars.
+      </p>
+      <p>
+        The earliest mathematical texts available are from Mesopotamia and
+        Egypt Plimpton 322 (Babylonian c. 2000 – 1900
+        BC), [2] the Rhind Mathematical Papyrus (Egyptian c. 1800 BC)[3] and
+        the Moscow Mathematical Papyrus (Egyptian
+        c. 1890 BC).
+        All of these texts mention the so-called Pythagorean triples, so,
+        by inference, the Pythagorean
+        theorem seems
+        to be the most ancient and widespread mathematical development after basic arithmetic and
+        geometry.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
